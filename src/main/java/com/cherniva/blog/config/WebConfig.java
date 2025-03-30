a8183d4b2e0af74d672da04d592581d29561ed18
Ivan Chernikov <chernikoviivan@gmail.com>
2025-03-30 17:58:10 +0200

    Change component scan base package and enable property source

--- src/main/java/com/cherniva/blog/config/WebConfig.java
+++ src/main/java/com/cherniva/blog/config/WebConfig.java
@@ -2,10 +2,12 @@ package com.cherniva.blog.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan("com.cherniva.blog.controller")
+@ComponentScan(basePackages = {"com.cherniva.blog"})
+@PropertySource("classpath:application.properties")
 public class WebConfig {
 }
