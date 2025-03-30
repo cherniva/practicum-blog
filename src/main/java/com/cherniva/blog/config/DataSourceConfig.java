815dd41bcf184b0e2aa8bd67b69ea5811b4c066d
Ivan Chernikov <chernikoviivan@gmail.com>
2025-03-30 18:24:24 +0200

    Correct DataSource configuration

--- src/main/java/com/cherniva/blog/config/DataSourceConfig.java
+++ src/main/java/com/cherniva/blog/config/DataSourceConfig.java
@@ -3,11 +3,9 @@ package com.cherniva.blog.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
-import java.sql.Driver;
 
 @Configuration
 public class DataSourceConfig {
@@ -20,20 +18,18 @@ public class DataSourceConfig {
     @Value("${spring.datasource.password}")
     private String password;
 
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(Driver.class.getName());
         dataSource.setUrl(url);
         dataSource.setUsername(username);
         dataSource.setPassword(password);
+        dataSource.setDriverClassName(driverClassName);
 
         return dataSource;
-    }
-
-    @Bean
-    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-        return new JdbcTemplate(dataSource);
     }
 
 //    @EventListener
