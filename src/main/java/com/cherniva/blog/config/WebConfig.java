ed42021a1bd385688e606f474e089ae1f0818753
Ivan Chernikov <chernikoviivan@gmail.com>
2025-03-30 14:05:56 +0200

    Implement simple basic controller and config. Configure servlet. Add initial dependencies and add initial project structure

---
+++ src/main/java/com/cherniva/blog/config/WebConfig.java
@@ -0,0 +1,11 @@
+package com.cherniva.blog.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.cherniva.blog.controller")
+public class WebConfig {
+}
