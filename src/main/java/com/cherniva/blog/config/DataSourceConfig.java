c13ab43ceb4e092c3825809f25c667b04f3233bc
Ivan Chernikov <chernikoviivan@gmail.com>
2025-03-30 17:57:35 +0200

    Add data source and thymeleaf configurations

---
+++ src/main/java/com/cherniva/blog/config/DataSourceConfig.java
@@ -0,0 +1,47 @@
+package com.cherniva.blog.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Driver;
+
+@Configuration
+public class DataSourceConfig {
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(Driver.class.getName());
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+//    @EventListener
+//    public void populate(ContextRefreshedEvent event) {
+//        DataSource dataSource = event.getApplicationContext().getBean(DataSource.class);
+//
+//        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+//        populator.addScript(new ClassPathResource("schema.sql"));
+//        populator.execute(dataSource);
+//    }
+}
