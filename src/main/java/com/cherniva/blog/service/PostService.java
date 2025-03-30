60ff0aa8c5fa4dc82ba49e092128ba7b85cb31cb
Ivan Chernikov <chernikoviivan@gmail.com>
2025-03-30 18:42:59 +0200

    Implement PostService

---
+++ src/main/java/com/cherniva/blog/service/PostService.java
@@ -0,0 +1,33 @@
+package com.cherniva.blog.service;
+
+import com.cherniva.blog.model.Post;
+import com.cherniva.blog.model.Tag;
+import com.cherniva.blog.repo.PostRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+    private final PostRepository postRepository;
+
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public Post save(Post post) {
+        return postRepository.save(post);
+    }
+
+    public Optional<Post> findById(Long id) {
+        return postRepository.findById(id);
+    }
+
+    public List<Post> findByTags(List<Tag> tags) {
+        List<Long> tagIds = tags.stream()
+                .map(Tag::getId)
+                .toList();
+        return postRepository.findByTags(tagIds);
+    }
+}
