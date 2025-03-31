90c7a8a30f28d2033b473a2d70359508c2b9fa38
Ivan Chernikov <chernikoviivan@gmail.com>
2025-03-31 19:10:16 +0200

    Add test to check if context loads

---
+++ src/test/java/com/cherniva/blog/service/PostServiceTest.java
@@ -0,0 +1,26 @@
+package com.cherniva.blog.service;
+
+import com.cherniva.blog.config.TestConfig;
+import com.cherniva.blog.config.WebConfiguration;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = WebConfiguration.class)
+class PostServiceTest {
+
+    @Autowired
+    private PostService postService;
+
+    @Test
+    public void contextLoads() {
+    }
+}
