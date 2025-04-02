b4f9d3be7a3e0099aec7479e8999b77d63cc0ddd
Ivan Chernikov <chernikoviivan@gmail.com>
2025-04-02 18:49:39 +0200

    Remove post list from tag

--- src/main/java/com/cherniva/blog/model/Tag.java
+++ src/main/java/com/cherniva/blog/model/Tag.java
@@ -4,13 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Tag {
     private Long id;
     private String tag;
-    private List<Long> postIds;
 }
