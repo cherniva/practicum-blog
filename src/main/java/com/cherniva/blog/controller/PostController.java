0070a3807f0409a2e42090865499223748f0b81e
Ivan Chernikov <chernikoviivan@gmail.com>
2025-04-02 19:44:11 +0200

    Implement pagination

--- src/main/java/com/cherniva/blog/controller/PostController.java
+++ src/main/java/com/cherniva/blog/controller/PostController.java
@@ -6,6 +6,7 @@ import com.cherniva.blog.service.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -19,17 +20,35 @@ public class PostController {
         this.postDtoConverter = postDtoConverter;
     }
 
-    @GetMapping({"", "/", "/posts"})
-    public String allPosts(Model model) {
-        List<PostDto> postDtos = postService.findAll().stream()
+    @GetMapping({"", "/", "/home", "/posts"})
+    public String allPosts(Model model,
+                           @RequestParam(name = "pageNumber", defaultValue = "1") int pageNumber,
+                           @RequestParam(name = "pageSize", defaultValue = "10") int pageSize) {
+                        //    @RequestParam(name = "sortBy", defaultValue = "id") String sortBy, // TODO: implement sorting
+                        //    @RequestParam(name = "sortDirection", defaultValue = "desc") String sortDirection
+        List<PostDto> postDtos = postService.findAllWithPagination(pageNumber, pageSize, "id", "ASC").stream()
                 .map(postDtoConverter::postToPostDto)
                 .toList();
+        long totalPosts = postService.countTotalPosts();
         model.addAttribute("posts", postDtos);
+        model.addAttribute("paging", new Paging(pageNumber, pageSize, totalPosts));
+        // model.addAttribute("sortBy", sortBy);
+        // model.addAttribute("sortDirection", sortDirection);
         return "posts";
     }
 
     @GetMapping("/posts/add")
     public String addPost() {
         return "add-post";
+    }
+
+    private record Paging(int pageNumber, int pageSize, long totalItems) {
+        public boolean hasPrevious() {
+            return pageNumber > 1;
+        }
+
+        public boolean hasNext() {
+            return pageNumber * pageSize < totalItems;
+        }
     }
 }
