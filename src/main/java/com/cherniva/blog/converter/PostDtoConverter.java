368975b0f9b2d229462ad4744e97f04419b17863
Ivan Chernikov <chernikoviivan@gmail.com>
2025-04-02 19:00:44 +0200

    Add postDto and post to postDto converter service

---
+++ src/main/java/com/cherniva/blog/converter/PostDtoConverter.java
@@ -0,0 +1,54 @@
+package com.cherniva.blog.converter;
+
+import com.cherniva.blog.dto.PostDto;
+import com.cherniva.blog.model.Comment;
+import com.cherniva.blog.model.Post;
+import com.cherniva.blog.model.Tag;
+import com.cherniva.blog.service.CommentService;
+import com.cherniva.blog.service.TagService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostDtoConverter {
+    private final CommentService commentService;
+    private final TagService tagService;
+
+    public PostDtoConverter(CommentService commentService, TagService tagService) {
+        this.commentService = commentService;
+        this.tagService = tagService;
+    }
+
+    public PostDto postToPostDto(Post post) {
+        Long postId = post.getId();
+        List<Comment> comments = commentService.findByPostId(postId);
+        List<Tag> tags = tagService.findByPostId(postId);
+
+        String postText = post.getText();
+        String preview = getFirstParagraph(postText);
+
+        PostDto postDto = new PostDto();
+        postDto.setPostId(postId);
+        postDto.setTitle(post.getTitle());
+        postDto.setTextPreview(preview);
+        postDto.setText(postText);
+        postDto.setImage(postDto.getImage());
+        postDto.setLikesCount(post.getLikes());
+        postDto.setComments(comments);
+        postDto.setTags(tags);
+
+        return postDto;
+    }
+
+    private String getFirstParagraph(String text) {
+        if (text == null || text.trim().isEmpty()) {
+            return "";
+        }
+
+        // Split by multiple possible paragraph separators
+        String[] paragraphs = text.split("(?m)(?:\r\n|\n|\r){2,}");
+        return paragraphs[0].trim();
+    }
+
+}
---
+++ src/main/java/com/cherniva/blog/dto/PostDto.java
@@ -0,0 +1,23 @@
+package com.cherniva.blog.dto;
+
+import com.cherniva.blog.model.Comment;
+import com.cherniva.blog.model.Tag;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostDto {
+    private Long postId;
+    private String title;
+    private String textPreview;
+    private String text;
+    private byte[] image;
+    private int likesCount;
+    private List<Comment> comments;
+    private List<Tag> tags;
+}
