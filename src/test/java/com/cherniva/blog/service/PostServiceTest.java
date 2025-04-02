accdbfa5d943d4495fb381fe2623786a913b5804
Ivan Chernikov <chernikoviivan@gmail.com>
2025-04-02 20:58:07 +0200

    Optimize imports

--- src/test/java/com/cherniva/blog/service/PostServiceTest.java
+++ src/test/java/com/cherniva/blog/service/PostServiceTest.java
@@ -1,16 +1,12 @@
 package com.cherniva.blog.service;
 
-import com.cherniva.blog.config.TestConfig;
 import com.cherniva.blog.config.WebConfiguration;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @WebAppConfiguration
