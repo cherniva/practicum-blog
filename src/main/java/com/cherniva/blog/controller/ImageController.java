d25ecc6b4d266404e8c05e8cdbe7926f8409a2c7
Ivan Chernikov <chernikoviivan@gmail.com>
2025-04-04 15:13:16 +0200

    Remove test code

--- src/main/java/com/cherniva/blog/controller/ImageController.java
+++ src/main/java/com/cherniva/blog/controller/ImageController.java
@@ -1,10 +1,7 @@
 package com.cherniva.blog.controller;
 
 import com.cherniva.blog.model.Image;
-import com.cherniva.blog.model.Post;
 import com.cherniva.blog.repo.ImageRepository;
-import com.cherniva.blog.repo.PostRepository;
-import com.cherniva.blog.service.PostService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -13,12 +10,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.util.InvalidUrlException;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/images")
@@ -31,28 +25,10 @@ public class ImageController {
 
     @GetMapping("/{id}")
     public ResponseEntity<byte[]> getImage(@PathVariable("id") Long postId) throws IOException {
-        Optional<Image> imageOpt = imageRepository.findByPostId(postId);
-
-        if (imageOpt.isEmpty()) {
-            Path imagePath = Paths.get("/Users/vokinrehc/Pictures/bird.jpg");
-
-            if (!Files.exists(imagePath)) {
-                throw new IOException("Image file not found: " + imagePath);
-            }
-
-            byte[] imageBytes = Files.readAllBytes(imagePath);
-
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.IMAGE_JPEG);
-            headers.setContentLength(imageBytes.length);
-
-            return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
-        } else {
-            Image image = imageOpt.get();
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.IMAGE_JPEG);
-            headers.setContentLength(image.getImage().length);
-            return new ResponseEntity<>(image.getImage(), headers, HttpStatus.OK);
-        }
+        Image image = imageRepository.findByPostId(postId).orElseThrow(() -> new InvalidUrlException("Post does not exist"));
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_JPEG);
+        headers.setContentLength(image.getImage().length);
+        return new ResponseEntity<>(image.getImage(), headers, HttpStatus.OK);
     }
 }
